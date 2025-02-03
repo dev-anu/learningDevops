6ac3fa9a4d02f0c4169bf4897150281f87f4b2d4
dev-anu <anu.infy850@gmail.com>
2025-02-03 09:16:18 +0530

    optimizing the code

--- day1/system_monitor.sh
+++ day1/system_monitor.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-echo "System Usage Report"
-echo "-------------------"
-echo "CPU Load:"
-uptime
-echo "Memory Usage:"
-free -m
-echo "Disk Usage:"
-df -h
+
+echo "📊 System Monitoring Script"
+echo "----------------------------"
+echo "📅 Date & Time: $(date)"
+echo ""
+
+echo "🔥 CPU Usage:"
+top -b -n1 | awk '/Cpu/ {print "CPU Load: " 100-$8"%"}'
+echo ""
+
+echo "💾 Memory Usage:"
+free -m | awk 'NR==2{printf "Used: %sMB / Total: %sMB (%.2f%%)\n", $3, $2, $3*100/$2 }'
+echo ""
+
+echo "👤 Logged-in Users:"
+who
+echo "----------------------------"

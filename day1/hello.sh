e863ca0fe42fe6c98fe75e1a058123ae503df28f
Anurag Tripathi <anuragt@wekan.company>
2025-02-03 14:40:46 +0530

    added hello world program

---
+++ day1/hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello world"
+
+# first line- tells the cmd which shell to use "#!/bin/bash"
+# second line - echo - prints output to the terminal
+#How to run script - 1) permit to execute - chmod +x file.sh 2)run- ./file.sh
+#second way to run - bash file.sh
+ 
